6ea50f3e0f8bcef5b77a2f83ca8ec0538a1d68c1
Meo9y <34277180+Meo9y@users.noreply.github.com>
2020-04-02 14:29:26 +0800

    Update File_byte_reverse.py

--- File_byte_reverse.py
+++ File_byte_reverse.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-import argparse
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-f', '--file', type=str, required=True,  help='Please input filename. eg. -f exp.jpg or C:\\exp.jpg')
